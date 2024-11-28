512e1abc12f05cbe88320e6060f2d0540e9aa4ff
mathsman5133 <mathsman5132@gmail.com>
2024-11-28 16:07:37 +1100

    cat instead of echo....

--- stage0/03-doover-docker/00-run.sh
+++ stage0/03-doover-docker/00-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 mkdir -p "${ROOTFS_DIR}/etc/apt/keyrings"
-echo files/docker.gpg.key > "${ROOTFS_DIR}/etc/apt/keyrings/docker.asc"
+cat files/docker.gpg.key > "${ROOTFS_DIR}/etc/apt/keyrings/docker.asc"
 chmod a+r /etc/apt/keyrings/docker.asc
 
 echo "deb [arch=${ARCH} signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian ${RELEASE} stable" > "${ROOTFS_DIR}/etc/apt/sources.list.d/docker.list"
