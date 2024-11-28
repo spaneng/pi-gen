66f2501e0c0d523fbfe7166db669c5f35fbdafb0
mathsman5133 <mathsman5132@gmail.com>
2024-11-28 15:30:07 +1100

    make keyrings directory

--- stage0/03-doover-docker/00-run.sh
+++ stage0/03-doover-docker/00-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 cat files/docker.gpg.key | gpg --dearmor > "${STAGE_WORK_DIR}/docker.gpg"
+mkdir -p "${ROOTFS_DIR}/etc/apt/keyrings"
 install -m 644 "${STAGE_WORK_DIR}/docker.gpg" "${ROOTFS_DIR}/etc/apt/keyrings/docker.asc"
 
 echo "deb [arch=${ARCH} signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian ${RELEASE} stable" > "${ROOTFS_DIR}/etc/apt/sources.list.d/docker.list"
