9030978b82231b54bcb95c46c37f20feb9c09015
mathsman5133 <mathsman5132@gmail.com>
2024-11-28 15:52:01 +1100

    fix docker pub keys

--- stage0/03-doover-docker/00-run.sh
+++ stage0/03-doover-docker/00-run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-cat files/docker.gpg.key | gpg --dearmor > "${STAGE_WORK_DIR}/docker.gpg"
 mkdir -p "${ROOTFS_DIR}/etc/apt/keyrings"
-install -m 644 "${STAGE_WORK_DIR}/docker.gpg" "${ROOTFS_DIR}/etc/apt/keyrings/docker.asc"
+echo files/docker.gpg.key > "${ROOTFS_DIR}/etc/apt/keyrings/docker.asc"
+chmod a+r /etc/apt/keyrings/docker.asc
 
 echo "deb [arch=${ARCH} signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian ${RELEASE} stable" > "${ROOTFS_DIR}/etc/apt/sources.list.d/docker.list"
 on_chroot <<- \EOF
