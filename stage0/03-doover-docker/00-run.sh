b9bdb4056d669874369d916006d72a9a175c84ca
mathsman5133 <mathsman5132@gmail.com>
2024-11-28 16:24:02 +1100

    don't run apt-get install

--- stage0/03-doover-docker/00-run.sh
+++ stage0/03-doover-docker/00-run.sh
@@ -7,5 +7,4 @@ chmod a+r /etc/apt/keyrings/docker.asc
 echo "deb [arch=${ARCH} signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian ${RELEASE} stable" > "${ROOTFS_DIR}/etc/apt/sources.list.d/docker.list"
 on_chroot <<- \EOF
   apt-get update
-  sudo apt-get install
 EOF
