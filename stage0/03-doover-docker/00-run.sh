04f5b9115a1ac90b6f558bc203fe7b748e00852f
mathsman5133 <mathsman5132@gmail.com>
2024-11-29 13:38:17 +1100

    wrap chmod command with rootfs directory

--- stage0/03-doover-docker/00-run.sh
+++ stage0/03-doover-docker/00-run.sh
@@ -2,10 +2,10 @@
 
 mkdir -p "${ROOTFS_DIR}/etc/apt/keyrings"
 cat files/docker.gpg.key > "${ROOTFS_DIR}/etc/apt/keyrings/docker.asc"
-chmod a+r /etc/apt/keyrings/docker.asc
+chmod a+r "${ROOTFS_DIR}/etc/apt/keyrings/docker.asc"
 
 cat files/doover.gpg.key > "${ROOTFS_DIR}/etc/apt/keyrings/doover.asc"
-chmod a+r /etc/apt/keyrings/doover.asc
+chmod a+r "${ROOTFS_DIR}/etc/apt/keyrings/doover.asc"
 
 echo "deb [arch=${ARCH} signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian ${RELEASE} stable" > "${ROOTFS_DIR}/etc/apt/sources.list.d/docker.list"
 echo "deb [arch=${ARCH} signed-by=/etc/apt/keyrings/doover.asc] http://apt.u.doover.com stable main" > "${ROOTFS_DIR}/etc/apt/sources.list.d/doover.list"
