17985b71630a55baca65d48105cec7fcae3edbaa
mathsman5133 <mathsman5132@gmail.com>
2024-11-29 18:50:26 +1100

    move docker installation to stage 2, don't build a stage 5 image

--- stage2/03-doover/01-run.sh
+++ stage2/03-doover/01-run.sh
@@ -53,3 +53,11 @@ gpio=21=op,dh
 dtoverlay=gpio-poweroff,gpiopin=6
 dtparam=i2c_arm=on
 EOF
+
+on_chroot <<- EOF
+  usermod -aG docker $FIRST_USER_NAME
+  ufw allow 22
+  ufw allow 80
+  ufw allow 443
+  ufw --force enable
+EOF
